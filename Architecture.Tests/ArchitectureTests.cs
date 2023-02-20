e2b3c46e1b619bb5b8d1094006cb3e24b2ef4d60
Jay Johnston <Jay.Johnston@Lynkz.com.au>
2023-02-20 11:36:42 +1100

    Added API Project

---
+++ Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,11 @@
+namespace Architecture.Tests
+{
+    public class ArchitectureTests
+    {
+        private const string DomainNamespace = "Domian";
+        private const string ApplicationNamespace = "Application";
+        private const string InfrastuctureNamespace = "Infrastucture";
+        private const string PresentationNamespace = "Presentation";
+        private const string WebNamespace = "Web";
+    }
+}
