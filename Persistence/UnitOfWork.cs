c9156ae79a7b26e4bcde8b164cdcb7f32ba174d2
Jay Johnston <Jay.Johnston@Lynkz.com.au>
2023-02-22 09:07:33 +1100

    Unit of work

---
+++ Persistence/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Repositories;
+
+namespace Persistence;
+
+internal sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UnitOfWork(ApplicationDbContext dbContext)
+    {
+        _dbContext=dbContext;
+    }
+
+    public Task<int> SaveChanagesAsync(CancellationToken cancellationToken = default)
+    {
+        return _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
